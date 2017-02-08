43be1d9e5a21959841959d52a8f62d7228fe6ace
David Ross <david@djr.com>
2017-02-07 19:19:35 -0500

    begin with fork of pde site

---
+++ cache/refreshCache.py
@@ -0,0 +1,58 @@
+#coding=utf-8
+
+# Download the google spreadsheet as a CSV using CURL
+# Convert the CSV file to a JSON variable, which I can call using <script>
+
+# requires CURL and Python
+import csv
+import json
+import os
+import string
+import subprocess
+
+# set the URL that we want to update from
+remoteURL = "https://docs.google.com/spreadsheets/d/1XVO1smiI3iOoLJt1nPkmnxqWb2Zi4okiDTtghdBfUcQ/export?gid=0&format=csv"
+# set the paths for the local files
+base = os.path.split(__file__)[0]
+csvPath = os.path.join(base, 'cache.csv')
+jsonPath = os.path.join(base, 'cache.json')
+
+# if we already aren't in the working directory, let's go there
+if base:
+    os.chdir(base)
+
+# use the subprocess module to run CURL from the command line
+proc = subprocess.Popen(['curl', remoteURL, '-o', 'cache.csv'], stdout=subprocess.PIPE)
+out = proc.communicate()[0]
+
+# get two file objects, one to read from and one to write to
+csvfile = open(csvPath, 'r')
+jsonfile = open(jsonPath, 'w')
+
+# determine the field names by reading the first line of csv and splitting it by a comma
+# we need the fieldnames in order to read the CSV
+#fieldnames = tuple(string.strip(csvfile.readlines()[0]).split(','))
+
+# recreate the CSV file, I don't know why
+csvfile = open(os.path.join(base, 'cache.csv'), 'r')
+
+fieldnames = ('Name', 'Category', 'Organization', 'Date', 'Application Deadline', 'Location', 'Fee/Free and/or Stipend', 'Description', 'URL', 'Graduate Credit', 'Students too?', 'Emily: \xf0\x9f\x91\x8d')
+
+# read the CSV, loop through it, and dump the JSON for each row
+reader = csv.DictReader(csvfile, fieldnames)
+
+# we are actually not making a json file, but a javascript variable
+# it's a little hacky but it's easy, it works, and doesn't require jquery, so...
+
+jsonfile.write('var data = [')
+i = 0
+for row in reader:
+    # don't write the first row because it is just the table headers
+    if i != 0:
+        json.dump(row, jsonfile)
+        jsonfile.write(',\n')
+    i += 1
+
+jsonfile.write('];')
+
+print 'Updated cache for ' + str(i+1) + ' rows.'
